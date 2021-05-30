9f825c93e38eea9a7a91c2a2895bc2c32ee4b92a
Maintebot <84876358+maintebot@users.noreply.github.com>
2021-05-30 13:42:55 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -7,22 +7,6 @@ echo -e ""
 echo -e "For Debian 9 32 bit & 64 bit" | lolcat
 echo -e "For VPS with KVM and VMWare virtualization" | lolcat
 echo -e ""
-echo -e "Original script by :" | lolcat
-echo -e "* Fornesia" | lolcat
-echo -e "* Rzengineer" | lolcat
-echo -e "* Fawzya" | lolcat
-echo -e ""
-echo -e "Mod by Zuan We Lose" | lolcat
-echo -e ""
-echo -e "   ───█▒▒░░░░░░░░░▒▒█───  " | lolcat
-echo -e "   ────█░░█░░░░░█░░█────          " | lolcat
-echo -e "   ─▄▄──█░░░▀█▀░░░█──▄▄─           " | lolcat
-echo -e "   █░░█─▀▄░░░░░░░▄▀─█░░█   " | lolcat
-echo -e "   █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█     " | lolcat
-echo -e ""  █░░║║║╠─║─║─║║║║║╠─░░█     "  |  lolcat
-echo -e "   █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█ " | lolcat
-echo -e "   █---------------------JsT x SWJR HUNTERS------------------█  " | lolcat
-echo -e "   █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█   " | lolcat
-echo -e "                                      JST FREENET | lolcat
-echo -e ""
-echo -e " Join Telegram : @JSTFREENET" | lolcat
+echo -e " █▀▀█ █▀▀█ █▀▀▀█ █▀▀▀█ █▀▀▀█ " | lolcat
+echo -e " █░▄▄ █▄▄█ ▀▀▀▄▄ ▀▀▀▄▄ ▀▀▀▄▄ " | lolcat
+echo -e " █▄▄█ █░▒█ █▄▄▄█ █▄▄▄█ █▄▄▄█ " | lolcat
