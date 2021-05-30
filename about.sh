9be05a1d00e6fc75dd7a0f2f370998e0ac189ad7
Maintebot <84876358+maintebot@users.noreply.github.com>
2021-05-30 13:27:11 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN " | lolcat 
+echo -e ""
+echo -e "For Debian 9 32 bit & 64 bit" | lolcat
+echo -e "For VPS with KVM and VMWare virtualization" | lolcat
+echo -e ""
+echo -e "Original script by :" | lolcat
+echo -e "* Fornesia" | lolcat
+echo -e "* Rzengineer" | lolcat
+echo -e "* Fawzya" | lolcat
+echo -e ""
+echo -e "Mod by Zuan We Lose" | lolcat
+echo -e ""
+echo -e "   ───█▒▒░░░░░░░░░▒▒█───  " | lolcat
+echo -e "   ────█░░█░░░░░█░░█────          " | lolcat
+echo -e "   ─▄▄──█░░░▀█▀░░░█──▄▄─           " | lolcat
+echo -e "   █░░█─▀▄░░░░░░░▄▀─█░░█   " | lolcat
+echo -e "   █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█     " | lolcat
+echo -e ""  █░░║║║╠─║─║─║║║║║╠─░░█     "  |  lolcat
+echo -e "   █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█ " | lolcat
+echo -e "   █---------------------JsT x SWJR HUNTERS------------------█  " | lolcat
+echo -e "   █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█   " | lolcat
+echo -e "                                      JST FREENET | lolcat
+echo -e ""
+echo -e " Join Telegram : @JSTFREENET" | lolcat
